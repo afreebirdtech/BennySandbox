e8406922dbe951986bfe0ba41105b0be5086e6cd
Deckins <bennythai2013@hotmail.com>
2019-05-13 18:15:05 -0400

    Created skeleton views for the Pages

--- sandbox/tables/urls.py
+++ sandbox/tables/urls.py
@@ -4,5 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.table_list, name='table_list'),
     path('tutors', views.tutor_list, name='tutor_list'),
+    path('about', views.about, name='about'),
+    path('affiliate', views.affiliate, name='affiliate'),
+    path('donate', views.donate, name='donate'),
+    path('events', views.events, name='events'),
+    path('fly_free', views.fly_free, name='fly_free'),
+    path('home', views.home, name='home'),
+    path('log_in', views.log_in, name='log_in'),
+    path('volunteers', views.volunteer_list, name='volunteer_list'),
+
+    
+
 
 ]
--- sandbox/tables/views.py
+++ sandbox/tables/views.py
@@ -7,3 +7,21 @@ def table_list(request):
 def tutor_list(request):
     tutors = Tutor.objects.all()
     return render(request,'tables/tutor_list.html', {'tutors': tutors})
+
+def about(request):
+    return render(request, 'tables/about.html', {})
+def affiliate(request):
+    return render(request, 'tables/affiliate.html', {})
+    
+def donate(request):
+    return render(request, 'tables/donate.html', {})
+def events(request):
+    return render(request, 'tables/events.html', {})
+def fly_free(request):
+    return render(request, 'tables/fly_free.html', {})
+def home(request):
+    return render(request, 'tables/home.html', {})
+def log_in(request):
+    return render(request, 'tables/log_in.html', {})
+def volunteer_list(request):
+    return render(request, 'tables/volunteer_list.html', {})
